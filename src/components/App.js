9f02148ba78ee40905d17cefd475fe00e066827f
Ksenia Komisarova <cardavar.ksen@gmail.com>
2022-10-11 02:53:51 +0300

    add

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import Footer from "./Footer/Footer";
+import Main from "./Main";
+import PageContainer from "./PageContainer";
+
+
+function App() {
+  return (
+    <>
+    <PageContainer>
+    <Main/>
+    </PageContainer>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
